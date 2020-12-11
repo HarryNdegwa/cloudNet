dd37b3a1f687f1763fb2cf092e1ef4f4874177cb
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-11 16:34:29 +0300

    Removed code redundancy

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -172,16 +172,3 @@ function Dashboard(props) {
 }
 
 export default Dashboard;
-
-{
-  /* <div className="dashboard">
-<DashboardHeader />
-<div className="dashboard-wrapper container-fluid">
-  <div className="row">
-    <div className="col-md-3 col-lg-2 dashboard-left">
-    <DashboardSidebar /></div>
-    <div className="col-md-9 col-lg-10 dashboard-right"></div>
-  </div>
-</div>
-</div> */
-}
